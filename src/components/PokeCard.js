ae7c0e1561be71950f8d5b43fa845363c9494c23
mrise <mrise@mrise.local>
2022-01-31 22:02:57 -0300

    add navbar and pokemon logo

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect, useState } from "react";
 import SingleCard from "./SingleCard";
+import NavBar from "./NavBar";
 import Grid from '@mui/material/Grid';
 
 const PokeCard = () => {
@@ -12,23 +13,22 @@ const PokeCard = () => {
     const pokeapi = async() => {
         const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100', {method: 'GET'})
         const apiresponse = await response.json()
-        console.log(apiresponse)
         setPokecard(apiresponse.results) 
        
     }
     return(
         <Fragment>
-            <h1>HOLA</h1>
+            <NavBar />
             <Grid container sx={{ justifyContent: 'center' }}>
                 <Grid item xs={10}>
                     <Grid container spacing={2}>
                         {
-                            pokecard.map(card =>{
+                            pokecard.map((card, index) =>{
                                 return(
-                                    <Grid item lg={2} xs={12} md={4}>
+                                    <Grid item key={index} lg={2} xs={12} md={4} sm={6}>
                                         <SingleCard
                                             container
-                                            key={card.name} 
+                                            uniqueKey={index} 
                                             pokename={card.name}
                                             pokeurl={card.url}>
                                         </SingleCard>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect, useState} from "react";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 import axios from "axios";
 import PokeImage from './PokeImage'
 
@@ -9,31 +10,34 @@ const SingleCard = ({pokename, pokeurl}) => {
 
     const [pokeInfo, setPokeInfo] = useState([])
 
-    useEffect(() => {
+    useEffect(()=>{
+        const singlePokeInfo = () => {
+            axios.get(`https://pokeapi.co/api/v2/pokemon/${pokename}`)
+                .then((response) => {
+                    const apiresponse = response.data;
+                    setPokeInfo(apiresponse);
+                }
+            )
+        }
         singlePokeInfo()
-    },[])
+    },[pokename])
 
-    const singlePokeInfo = () =>{
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokename}`)
-            .then((response) => {
-                console.log(response.data)
-                const apiresponse = response.data
-                setPokeInfo(apiresponse)
-        })
-    }
+    
 
     return(
         <Fragment>
-            <Card >
-                <PokeImage urlSprite={pokeInfo.sprites}/>
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {pokename}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {pokeurl}
-                    </Typography>
-                </CardContent>
+            <Card>
+                <CardActionArea>
+                    <PokeImage urlSprite={pokeInfo.sprites}/>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {pokename}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {pokeurl}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
             </Card>
         </Fragment>
     )
